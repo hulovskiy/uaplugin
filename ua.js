b502f5e1e668af4205b6ffa7717ef997714c2fde
hulovskiy <137083815+hulovskiy@users.noreply.github.com>
2025-03-25 13:31:06 +0200

    Update ua.js

--- ua.js
+++ ua.js
@@ -2,53 +2,63 @@
     'use strict';
 
     var Uakino = {
-        base_url: 'https://uakino.me', // Основний URL сайту
+        base_url: 'https://uakino.me',
 
-        // Функція пошуку контенту
         search: function (query, callback) {
-            var url = this.base_url + '/search?query=' + encodeURIComponent(query); // Замініть на реальний URL пошуку
+            var url = this.base_url + '/search?q=' + encodeURIComponent(query); // Перевірте правильність URL
             network.silent(url, function (result) {
                 var items = [];
-                // Парсинг результатів пошуку (залежить від HTML uakino.me)
-                var html = $('<div>').html(result);
-                html.find('.film-item').each(function () { // Замініть '.film-item' на реальний селектор
-                    var title = $(this).find('.title').text(); // Приклад селектора
-                    var link = $(this).find('a').attr('href');
-                    items.push({
-                        title: title,
-                        url: Uakino.base_url + link,
-                        type: 'movie' // або 'series'
+                try {
+                    var html = $('<div>').html(result);
+                    html.find('.shortstory').each(function () { // Приклад селектора для uakino.me
+                        var title = $(this).find('.shortstory-title a').text() || 'Без назви';
+                        var link = $(this).find('.shortstory-title a').attr('href') || '';
+                        if (title && link) {
+                            items.push({
+                                title: title.trim(),
+                                url: Uakino.base_url + link,
+                                type: 'movie' // або 'series', залежно від контенту
+                            });
+                        }
                     });
-                });
-                callback(items);
+                    callback(items.length ? items : []);
+                } catch (e) {
+                    console.log('Uakino search error:', e);
+                    callback([]);
+                }
             }, function () {
-                callback([]); // У разі помилки
+                callback([]);
             });
         },
 
-        // Функція для отримання даних про фільм/серіал
         parse: function (url, callback) {
             network.silent(url, function (result) {
-                var html = $('<div>').html(result);
-                var video_urls = [];
-                // Приклад парсингу посилань на відео
-                html.find('.player source').each(function () { // Замініть селектор
-                    video_urls.push({
-                        quality: $(this).attr('data-quality') || 'HD',
-                        url: $(this).attr('src')
+                try {
+                    var html = $('<div>').html(result);
+                    var video_urls = [];
+                    html.find('.player video source').each(function () { // Адаптуйте селектор
+                        var src = $(this).attr('src');
+                        if (src) {
+                            video_urls.push({
+                                quality: $(this).attr('data-quality') || 'HD',
+                                url: src
+                            });
+                        }
                     });
-                });
-                callback({
-                    streams: video_urls,
-                    title: html.find('h1').text() // Приклад
-                });
+                    callback({
+                        streams: video_urls.length ? video_urls : [{ quality: 'HD', url: '' }],
+                        title: html.find('h1').text() || 'Без назви'
+                    });
+                } catch (e) {
+                    console.log('Uakino parse error:', e);
+                    callback(null);
+                }
             }, function () {
                 callback(null);
             });
         }
     };
 
-    // Реєстрація плагіну в Lampa
     Lampa.Component.add('uakino', {
         name: 'Uakino',
         search: function (query, callback) {
@@ -59,7 +69,6 @@
         }
     });
 
-    // Додавання в меню Lampa
     Lampa.Settings.add('online', {
         id: 'uakino',
         name: 'Uakino',
